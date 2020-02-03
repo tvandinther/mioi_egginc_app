9b0972d7b10660e08991325d488b5b7dbe9c2a37
tvandinther <tvandinther@gmail.com>
2020-02-03 17:39:13 +1300

    pwa

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
 import MyThemeProvider from "./MyThemeProvider"
 
 import App from "./App.js"
-import store from "./store"
+import store, { persistor } from "./store"
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('sw.js', {scope: "/", updateViaCache: 'none'}).then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+}
 
 ReactDOM.render(
-    <MyThemeProvider>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </MyThemeProvider>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <MyThemeProvider>
+                <App/>
+            </MyThemeProvider>
+        </PersistGate>
+    </Provider>
     , document.getElementById("root")
 )
