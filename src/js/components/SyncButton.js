72ee50ab887cf29a86a7a9a64e934be404da468b
tvandinther <tvandinther@gmail.com>
2020-06-12 15:35:27 +1200

    added analytics to sync action

--- src/js/components/SyncButton.js
+++ src/js/components/SyncButton.js
@@ -4,6 +4,7 @@ import { IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPlayerCoops } from "../tools"
+import ReactGA from "react-ga"
 
 // ACTIONS
 import { fetchNews } from "../actions/appActions"
@@ -39,6 +40,12 @@ export default function SyncButton(props) {
 		if (settings.playerId) dispatch(validatePlayerId(settings.playerId))
 		fetchPlayerCoops(coopIds, dispatch)
 		dispatch(fetchNews(5))
+
+		ReactGA.event({
+			category: "Interaction",
+			action: "Sync",
+			label: window.location.pathname,
+		})
 	}
 
 	const noAnimation = {
